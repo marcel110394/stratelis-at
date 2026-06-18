ce5e1119fe0a2811b5d886cca0b1972771b6259e
marcel110394 <aichmayr.marcel@gmail.com>
2026-06-18 21:28:11 +0200

    Rename script.js to js/script.js

---
+++ js/script.js
@@ -0,0 +1,1 @@
+const menuButton=document.querySelector('.menu-toggle');const nav=document.querySelector('.nav');if(menuButton&&nav){menuButton.addEventListener('click',()=>nav.classList.toggle('open'));nav.querySelectorAll('a').forEach(link=>link.addEventListener('click',()=>nav.classList.remove('open')));}
--- script.js
+++
@@ -1,1 +0,0 @@
-const menuButton=document.querySelector('.menu-toggle');const nav=document.querySelector('.nav');if(menuButton&&nav){menuButton.addEventListener('click',()=>nav.classList.toggle('open'));nav.querySelectorAll('a').forEach(link=>link.addEventListener('click',()=>nav.classList.remove('open')));}
